1fe6c63aa238cfd9de6292d9a9c2206c653ab6d2
Bruce Wang <yue.wang@door3.com>
2017-11-01 16:19:34 -0400

    Add gulp concat CSS plugin

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp      = require('gulp'),
       uglify    = require('gulp-uglify'),
       rename    = require('gulp-rename'),
       changed   = require('gulp-changed');
+      concat    = require('gulp-concat');
 
 const SCSS_SOURCE = './src/assets/**/*.scss';
 const SCSS_DEST = './src/assets/css';
@@ -13,7 +14,7 @@ gulp.task('compile_scss', function() {
   gulp.src('./src/assets/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(concat('default.min.css'))
     .pipe(changed(SCSS_DEST))
     .pipe(gulp.dest(SCSS_DEST))
 });
